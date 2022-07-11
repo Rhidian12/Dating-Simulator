35c13de75fbc08cd2d2383a6b0835241be1eb9fc
Rhidian De Wit <rhidiandewit@gmail.com>
2022-07-11 10:39:26 +0200

    clean up npc

--- Dating Simulator/Assets/Scripts/Characters/NPC.cs
+++ Dating Simulator/Assets/Scripts/Characters/NPC.cs
@@ -6,34 +6,44 @@ public class NPC : Character
 {
     public List<DialogueOption> DialogueOptions
     {
-        get => _dialogueOptions;
+        get => _DialogueOptions;
     }
     public int RelationWithPlayer
     {
-        get => _relationWithPlayer;
+        get => _RelationWithPlayer;
         set
         {
             if (value <= _MaxRelationshipValue && value >= _MinRelationshipValue)
-                _relationWithPlayer = value;
+            {
+                _RelationWithPlayer = value;
+            }
         }
     }
 
-    private List<DialogueOption> _dialogueOptions = new List<DialogueOption>();
-    private int _relationWithPlayer = 0;
+    private List<DialogueOption> _DialogueOptions = new List<DialogueOption>();
+    private int _RelationWithPlayer = 0;
+
+    private void Start()
+    {
+        GameObject.FindGameObjectWithTag("MinimalGame").GetComponent<NPCManager>().GetNPCByName(Name);
+
+        if (Name == null || Name.Length == 0)
+        {
+            Debug.LogError("NPC::Start() > NPC Name could not be found. Either MinimalGame or NPCManager are at fault.");
+        }
+    }
 
     public void AddDialogueOption(DialogueOption dialogueOption)
     {
         DialogueOptions.Add(dialogueOption);
     }
+
+    /* [TODO]: Check if this function is ever used? */
     public override void OnDialogueOptionSelected(PlayerDialogueOption dialogueOption)
     {
         foreach (IDialogueResult result in dialogueOption.Results)
         {
             result.Execute();
         }
-    }
-    private void Start()
-    {
-        GameObject.FindGameObjectWithTag("MinimalGame").GetComponent<NPCManager>().GetNPCByName(Name);
     }
 }
